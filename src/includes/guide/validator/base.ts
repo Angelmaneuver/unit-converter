b7e5b7bafcd07c9077b64d0944c808bed63ca0b3
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2022-02-11 14:38:42 +0900

    Bug fixed.

--- src/includes/guide/validator/base.ts
+++ src/includes/guide/validator/base.ts
@@ -10,7 +10,7 @@ export class BaseValidator {
 		const maximum: number = Optional.ofNullable(options?.maximum).orElseNonNullable(65555);
 		const string2Number   = Number(value);
 
-		if (value.length > 0 && (isNaN(string2Number) || string2Number > maximum || string2Number <= minimum)) {
+		if (0 === value.length || value.length > 0 && (isNaN(string2Number) || string2Number > maximum || string2Number <= minimum)) {
 			// eslint-disable-next-line @typescript-eslint/no-unused-vars
 			return new Promise<string>((resolve, reject) => {
 				resolve(`Enter a number between greater than ${minimum} and ${maximum} for ${name}.`);
